dc86d423541ecd671ee7aa4ddc5bf9527b2c7721
fullarray <sifreed77@gmail.com>
2017-09-15 02:16:41 -0400

    Testing Net Core 2.0 VB object-oriented features
    Testing Net Core 2.0 VB object-oriented features.

--- app1/Program.vb
+++ app1/Program.vb
@@ -2,6 +2,26 @@ Imports System
 
 Module Program
     Sub Main(args As String())
+        'Program p = New Program ()
         Console.WriteLine("Hello World!")
+        MyOne()
+        MyTwo()
     End Sub
+
+    Sub MyOne()
+        Dim digit as Integer
+        Dim stuff as String        
+        digit = 1
+
+        stuff = "Hello "
+        Console.Write(stuff)
+    End Sub
+
+    Sub MyTwo()
+        Dim digit as Integer
+        Dim stuff as String
+        digit = 2
+        stuff = "World!"
+        Console.Write(stuff)
+    End Sub            
 End Module
